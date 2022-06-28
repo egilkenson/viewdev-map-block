ad0df506c1ef07c068699e5b555fe07383bc41ed
Eben Gilkenson <eben@gilkenson.com>
2022-06-28 13:01:58 -0400

    Reordered variable declarations for efficency

--- src/save.js
+++ src/save.js
@@ -1,26 +1,28 @@
-import { useBlockProps } from '@wordpress/block-editor'
+import { useBlockProps } from '@wordpress/block-editor';
 
-const Save = (props) => {
-	const { className, attributes } = props
-	const style = { height: `${attributes.height}px` }
-	const blockProps = useBlockProps.save({
+const Save = ( props ) => {
+	const { className, attributes } = props;
+
+	if ( attributes.formatted_address === null ) {
+		return null;
+	}
+
+	const style = { height: `${ attributes.height }px` };
+	const blockProps = useBlockProps.save( {
 		className,
 		style,
-	})
+	} );
 
-	if (attributes.formatted_address === null) {
-		return null
-	}
 	return (
 		<div
-			{...blockProps}
-			data-lng={attributes.lng}
-			data-lat={attributes.lat}
-			data-zoom={attributes.zoom}
-			data-zoomcontrol={attributes.zoomControl}
+			{ ...blockProps }
+			data-lng={ attributes.lng }
+			data-lat={ attributes.lat }
+			data-zoom={ attributes.zoom }
+			data-zoomcontrol={ attributes.zoomControl }
 			data-target="vwd-map-div"
 		/>
-	)
-}
+	);
+};
 
-export default Save
+export default Save;
